c505f45d65b80bb23808c991c442090194014de3
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-08 16:01:31 +0300

    designing calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
@@ -6,16 +7,29 @@ const App = () => {
       <div className="container">
         <div className="screen"></div>
         <div className="keyboard">
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
+          <button>AC</button>
+          <button>DEL</button>
+          <button>%</button>
+          <button>/</button>
+
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>x</button>
+
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+
+          <button className="big">0</button>
+          <button>.</button>
+          <button>=</button>
         </div>
       </div>
     </>
