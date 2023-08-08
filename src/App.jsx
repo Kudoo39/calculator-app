1efe3e980c2b4a6ab85b25eedc14df76b1dd86ef
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-08 17:31:12 +0300

    adding handleClick function to show input after clicking

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [value, setValue] = useState("");
+
+  const handleClick = (e) => {
+    setValue(value + e.target.value);
+  };
+
   return (
-    <>
-      <div className="container">
-        <div className="screen"></div>
-        <div className="keyboard">
-          <button>AC</button>
-          <button>DEL</button>
-          <button>%</button>
-          <button>/</button>
-
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>x</button>
-
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-
-          <button className="big">0</button>
-          <button>.</button>
-          <button>=</button>
-        </div>
+    <div className="container">
+      <div className="screen">
+        <input type="text" value={value} readOnly />
       </div>
-    </>
+
+      <div className="keyboard">
+        <button onClick={handleClick} value="AC">
+          AC
+        </button>
+        <button onClick={handleClick} value="DEL">
+          DEL
+        </button>
+        <button onClick={handleClick} value="%">
+          %
+        </button>
+        <button onClick={handleClick} value="/">
+          /
+        </button>
+
+        <button onClick={handleClick} value="7">
+          7
+        </button>
+        <button onClick={handleClick} value="8">
+          8
+        </button>
+        <button onClick={handleClick} value="9">
+          9
+        </button>
+        <button onClick={handleClick} value="x">
+          x
+        </button>
+
+        <button onClick={handleClick} value="4">
+          4
+        </button>
+        <button onClick={handleClick} value="5">
+          5
+        </button>
+        <button onClick={handleClick} value="6">
+          6
+        </button>
+        <button onClick={handleClick} value="-">
+          -
+        </button>
+
+        <button onClick={handleClick} value="1">
+          1
+        </button>
+        <button onClick={handleClick} value="2">
+          2
+        </button>
+        <button onClick={handleClick} value="3">
+          3
+        </button>
+        <button onClick={handleClick} value="+">
+          +
+        </button>
+
+        <button onClick={handleClick} value="0" className="big">
+          0
+        </button>
+        <button onClick={handleClick} value=".">
+          .
+        </button>
+        <button onClick={handleClick} value="=">
+          =
+        </button>
+      </div>
+    </div>
   );
 };
 
