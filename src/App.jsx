b637989c63b1a737f2322caaa4209fbaf764dd53
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-08 15:51:54 +0300

    starting calculator-app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="screen"></div>
+        <div className="keyboard">
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
